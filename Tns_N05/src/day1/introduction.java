e1b2a587faf62d40bcf827ffda110bb7d5c6ee15
vaibhavwagh77 <84269035+vaibhavwagh77@users.noreply.github.com>
2023-09-24 07:11:55 -0700

    Introduction

---
+++ Tns_N05/src/day1/introduction.java
@@ -0,0 +1,33 @@
+package day1;
+import java.util.Scanner;
+
+public class introduction {
+	//program to demonstrate on user input
+	
+	
+		public static void main(String[] args)
+		{
+			// TODO Auto-generated method stub
+				Scanner s=new Scanner(System.in);
+			/*
+				
+				System.out.println("Enter the value of num:");
+				int num=s.nextInt();
+				System.out.println("The value of num is "+num);
+				
+				System.out.println("Enter the value of num:");
+				float num1=s.nextFloat();
+				System.out.println("The value of float num is "+num1);
+				
+				System.out.println("Enter the Character:");
+				char c=s.next().charAt(0);
+				System.out.println("The character  is "+c);
+				*/
+				System.out.println("Enter the String");
+				String str=s.nextLine();
+				System.out.println("The string  is "+str);
+		}
+
+	}
+
+
